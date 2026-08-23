4928f0831b7538a454b90d5ab446d175f2e4e50c
HauPhan <phuchau.nam2005@gmail.com>
2026-08-23 21:00:57 +0700

    fea: user send chat message

--- src/main/java/com/vn/smart_space/repository/ConversationRepository.java
+++ src/main/java/com/vn/smart_space/repository/ConversationRepository.java
@@ -2,16 +2,15 @@ package com.vn.smart_space.repository;
 
 import java.util.Optional;
 
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.vn.smart_space.model.Conversation;
-
-import org.springframework.data.repository.query.Param;
 
 @Repository
 public interface ConversationRepository extends JpaRepository<Conversation, String> {
@@ -23,5 +22,12 @@ public interface ConversationRepository extends JpaRepository<Conversation, Stri
     @EntityGraph(attributePaths = { "participants", "participants.user" })
     @Query("SELECT DISTINCT c FROM Conversation c JOIN c.participants p WHERE p.user.id = :userId ORDER BY c.lastMessageTime DESC NULLS LAST")
     Page<Conversation> findAllByUserId(@Param("userId") String userId, Pageable pageable);
+
+    // Check conversation exists and sender is member
+    @Query("SELECT c FROM Conversation " +
+            "WHERE c.id = :conversationId " +
+            "AND EXISTS(SELECT p FROM c.participants p WHERE p.user.id = :userId)")
+    Optional<Conversation> findByIdAndMember(@Param("conversationId") String conversationId,
+            @Param("userId") String userId);
 
 }
